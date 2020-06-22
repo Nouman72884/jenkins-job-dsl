85c6760eea16ffab9a002904e3b8cce4f1172911
root <root@ip-172-31-82-156.ec2.internal>
2020-06-22 17:31:12 +0000

    job dsl

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/Nouman72884/jenkins-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/Nouman72884/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('Nouman72884')
             node / gitConfigEmail('noumansatti7@gmail.com')
         }
