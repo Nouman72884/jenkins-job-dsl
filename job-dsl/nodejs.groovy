647f3d919f36956b7c50bde729053d8c21181e13
root <root@ip-172-31-82-156.ec2.internal>
2020-06-22 07:31:29 +0000

    job dsl

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/Nouman72884/jenkins-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Nouman72884')
+            node / gitConfigEmail('noumansatti7@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
